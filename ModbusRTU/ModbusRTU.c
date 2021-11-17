c9e777bce26ee2d3db22eb18662c803e3d74cfb7
Solderingironspb <olegdizzvolkov@yandex.ru>
2021-11-17 16:30:20 +0300

    ModbusRTU_Read_Holding_Registers

--- ModbusRTU/ModbusRTU.c
+++ ModbusRTU/ModbusRTU.c
@@ -31,9 +31,9 @@ uint16_t ModbusRTU_CRC16_Calculate(uint8_t *data, uint8_t lenght, uint8_t byte_o
 		crc ^= *data++;
 		for (int i = 0; i < 8; i++) {
 			if (crc & 0x01) {
-				crc = (crc >> 1) ^ 0xA001;
+				crc = (crc >> 1u) ^ 0xA001;
 			} else {
-				crc = crc >> 1;
+				crc = crc >> 1u;
 			}
 		}
 	}
@@ -41,21 +41,63 @@ uint16_t ModbusRTU_CRC16_Calculate(uint8_t *data, uint8_t lenght, uint8_t byte_o
 	case (0): //1234(младшим регистром вперед, младшим байтом вперед),
 		break;
 	case (1): //3412(старшим регистром вперед, младшим байтом вперед),
-		crc = (crc << 8) | (crc >> 8);
+		crc = (crc << 8u) | (crc >> 8u);
 		break;
 	case (2): //2143(младшим регистром вперед, старшим байтом вперед),
-		crc = (((crc >> 8) & 0x0F) << 12) | ((crc >> 12) << 8) | ((crc << 12) << 4) | ((crc >> 4) & 0x00F);
+		crc = (((crc >> 8u) & 0x0F) << 12u) | ((crc >> 12u) << 8u) | ((crc << 12u) << 4u) | ((crc >> 4u) & 0x00F);
 		break;
 	case (3): //4321(старшим регистром вперед, старшим байтом вперед).
-		crc = (((crc >> 8) & 0x0F) << 4) | (crc >> 12) | ((crc << 12) << 12) | (((crc >> 4) & 0x00F) << 8);
+		crc = (((crc >> 8u) & 0x0F) << 4u) | (crc >> 12u) | ((crc << 12u) << 12u) | (((crc >> 4u) & 0x00F) << 8u);
 		break;
 	}
 	return crc;
 }
 /*------------------------------CRC16(ModbusRTU)----------------------------------*/
 
-/*------------------------------Функция 00x03 Read Analog Output Holding Registers----------------------------- */
-void ModbusRTU_Read_Holding_Registers(uint8_t Slave_ID, uint16_t Read_adress, uint8_t Quantity, uint8_t Slave_byte_order) {
+/*------------------------------------Функция 00x01 Read Coils----------------------------------- */
+void ModbusRTU_Read_Coils_0x01(uint8_t Slave_ID, uint16_t Read_adress, uint16_t Quantity, uint8_t Slave_byte_order) {
+///Функция 0x01 Read Coils
+/// \param Slave_ID - ID Modbus RTU Slave устройства, к которому обращаемся
+/// \param Read_adress - Адрес, с которого начинаем чтение данных
+/// \param Quantity - количество запрашиваемых 16ти битных данных.
+	//Quantity должен быть от 1 до 2000.
+/// \param Slave_byte_order - Порядок регистра и байт в ModbusRTU Slave устройстве
+
+	ModbusRTU_tx_buffer[0] = Slave_ID;
+	ModbusRTU_tx_buffer[1] = 0x01;
+	ModbusRTU_tx_buffer[2] = (uint16_t) Read_adress >> 8u;
+	ModbusRTU_tx_buffer[3] = (uint16_t) Read_adress & 0x00FF;
+	ModbusRTU_tx_buffer[4] = (uint16_t) Quantity >> 8u;
+	ModbusRTU_tx_buffer[5] = (uint16_t) Quantity & 0x00FF;
+	uint16_t CRC16 = ModbusRTU_CRC16_Calculate(ModbusRTU_tx_buffer, 6, Slave_byte_order);
+	ModbusRTU_tx_buffer[6] = (uint16_t) CRC16 >> 8u;
+	ModbusRTU_tx_buffer[7] = (uint16_t) CRC16 & 0x00FF;
+}
+/*------------------------------------Функция 00x01 Read Coils----------------------------------- */
+
+/*--------------------------------Функция 00x02 Read Discrete Inputs----------------------------- */
+void ModbusRTU_Read_Discrete_Inputs_0x02(uint8_t Slave_ID, uint16_t Read_adress, uint16_t Quantity, uint8_t Slave_byte_order) {
+///Функция 0x02 Read Discrete Inputs
+/// \param Slave_ID - ID Modbus RTU Slave устройства, к которому обращаемся
+/// \param Read_adress - Адрес, с которого начинаем чтение данных
+/// \param Quantity - количество запрашиваемых 16ти битных данных.
+	//Quantity должен быть от 1 до 2000.
+/// \param Slave_byte_order - Порядок регистра и байт в ModbusRTU Slave устройстве
+
+	ModbusRTU_tx_buffer[0] = Slave_ID;
+	ModbusRTU_tx_buffer[1] = 0x02;
+	ModbusRTU_tx_buffer[2] = (uint16_t) Read_adress >> 8u;
+	ModbusRTU_tx_buffer[3] = (uint16_t) Read_adress & 0x00FF;
+	ModbusRTU_tx_buffer[4] = (uint16_t) Quantity >> 8u;
+	ModbusRTU_tx_buffer[5] = (uint16_t) Quantity & 0x00FF;
+	uint16_t CRC16 = ModbusRTU_CRC16_Calculate(ModbusRTU_tx_buffer, 6, Slave_byte_order);
+	ModbusRTU_tx_buffer[6] = (uint16_t) CRC16 >> 8u;
+	ModbusRTU_tx_buffer[7] = (uint16_t) CRC16 & 0x00FF;
+}
+/*--------------------------------Функция 00x02 Read Discrete Inputs----------------------------- */
+
+/*------------------------------Функция 00x03 Read Holding Registers----------------------------- */
+void ModbusRTU_Read_Holding_Registers_0x03(uint8_t Slave_ID, uint16_t Read_adress, uint8_t Quantity, uint8_t Slave_byte_order) {
 ///Функция 0x03 Read Holding Registers
 /// \param Slave_ID - ID Modbus RTU Slave устройства, к которому обращаемся
 /// \param Read_adress - Адрес, с которого начинаем чтение данных
@@ -73,5 +115,27 @@ void ModbusRTU_Read_Holding_Registers(uint8_t Slave_ID, uint16_t Read_adress, ui
 	ModbusRTU_tx_buffer[6] = (uint16_t) CRC16 >> 8u;
 	ModbusRTU_tx_buffer[7] = (uint16_t) CRC16 & 0x00FF;
 }
-/*------------------------------Функция 00x03 Read_adress Analog Output Holding Registers----------------------------- */
+/*------------------------------Функция 00x03 Read Holding Registers----------------------------- */
+
+/*--------------------------------Функция 00x04 Read Input Registers----------------------------- */
+void ModbusRTU_Read_Input_Registers_0x04(uint8_t Slave_ID, uint16_t Read_adress, uint8_t Quantity, uint8_t Slave_byte_order) {
+///Функция 0x04 Read Discrete Inputs
+/// \param Slave_ID - ID Modbus RTU Slave устройства, к которому обращаемся
+/// \param Read_adress - Адрес, с которого начинаем чтение данных
+/// \param Quantity - количество запрашиваемых 16ти битных данных.
+	//Quantity должен быть от 1 до 125.
+/// \param Slave_byte_order - Порядок регистра и байт в ModbusRTU Slave устройстве
+
+	ModbusRTU_tx_buffer[0] = Slave_ID;
+	ModbusRTU_tx_buffer[1] = 0x04;
+	ModbusRTU_tx_buffer[2] = (uint16_t) Read_adress >> 8u;
+	ModbusRTU_tx_buffer[3] = (uint16_t) Read_adress & 0x00FF;
+	ModbusRTU_tx_buffer[4] = 0x00;
+	ModbusRTU_tx_buffer[5] = Quantity;
+	uint16_t CRC16 = ModbusRTU_CRC16_Calculate(ModbusRTU_tx_buffer, 6, Slave_byte_order);
+	ModbusRTU_tx_buffer[6] = (uint16_t) CRC16 >> 8u;
+	ModbusRTU_tx_buffer[7] = (uint16_t) CRC16 & 0x00FF;
+}
+/*--------------------------------Функция 00x04 Read Input Registers----------------------------- */
+
 #endif /* SRC_MODBUSRTU_C_ */
--- ModbusRTU/ModbusRTU.h
+++ ModbusRTU/ModbusRTU.h
@@ -14,6 +14,9 @@
 #include <main.h>
 
 uint16_t ModbusRTU_CRC16_Calculate(uint8_t *data, uint8_t lenght, uint8_t byte_order);
-void ModbusRTU_Read_Holding_Registers(uint8_t Slave_ID, uint16_t Read_adress, uint8_t Quantity, uint8_t Slave_byte_order);
+void ModbusRTU_Read_Coils_0x01(uint8_t Slave_ID, uint16_t Read_adress, uint16_t Quantity, uint8_t Slave_byte_order); //Функция 0x01
+void ModbusRTU_Read_Discrete_Inputs_0x02(uint8_t Slave_ID, uint16_t Read_adress, uint16_t Quantity, uint8_t Slave_byte_order); //Функция 0x02
+void ModbusRTU_Read_Holding_Registers_0x03(uint8_t Slave_ID, uint16_t Read_adress, uint8_t Quantity, uint8_t Slave_byte_order); //Функция 0x03
+void ModbusRTU_Read_Input_Registers_0x04(uint8_t Slave_ID, uint16_t Read_adress, uint8_t Quantity, uint8_t Slave_byte_order); //Функция 00x04
 
 #endif /* INC_MODBUSRTU_H_ */
