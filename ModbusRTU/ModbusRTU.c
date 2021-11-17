4c3bc18a55e45ca91cb8184ab8d6ee1fe397c0fa
Solderingironspb <olegdizzvolkov@yandex.ru>
2021-11-17 10:35:59 +0300

    ModbusRTU_Read_Holding_Registers

--- ModbusRTU/ModbusRTU.c
+++ ModbusRTU/ModbusRTU.c
@@ -1,6 +1,9 @@
 /*
  * ModbusRTU.c
  *
+ *  Библиотека для работы по протоколу ModbusRTU в режиме Master.
+ *  Функции: формировать запрос, вычислять CRC16.
+ *
  *  Created on: Nov 16, 2021
  *      Author: Oleg Volkov
  */
@@ -9,6 +12,8 @@
 #define SRC_MODBUSRTU_C_
 
 #include "ModbusRTU.h"
+
+uint8_t ModbusRTU_tx_buffer[64] = { 0, };
 
 /*------------------------------CRC16(ModbusRTU)----------------------------------*/
 uint16_t ModbusRTU_CRC16_Calculate(uint8_t *data, uint8_t lenght, uint8_t byte_order) {
@@ -49,4 +54,24 @@ uint16_t ModbusRTU_CRC16_Calculate(uint8_t *data, uint8_t lenght, uint8_t byte_o
 }
 /*------------------------------CRC16(ModbusRTU)----------------------------------*/
 
+/*------------------------------Функция 00x03 Read Analog Output Holding Registers----------------------------- */
+void ModbusRTU_Read_Holding_Registers(uint8_t Slave_ID, uint16_t Read_adress, uint8_t Quantity, uint8_t Slave_byte_order) {
+///Функция 0x03 Read Holding Registers
+/// \param Slave_ID - ID Modbus RTU Slave устройства, к которому обращаемся
+/// \param Read_adress - Адрес, с которого начинаем чтение данных
+/// \param Quantity - количество запрашиваемых 16ти битных данных. Если мы запрашиваем Float, то Quantity кратно 2, т.е. 32 бита. 2 Float переменные - соответственно Quantity = 4. И т.д.
+	//Quantity должен быть от 1 до 125.
+/// \param Slave_byte_order - Порядок регистра и байт в ModbusRTU Slave устройстве
+
+	ModbusRTU_tx_buffer[0] = Slave_ID;
+	ModbusRTU_tx_buffer[1] = 0x03;
+	ModbusRTU_tx_buffer[2] = (uint8_t) (Read_adress >> 8);
+	ModbusRTU_tx_buffer[3] = (uint8_t) (Read_adress << 8);
+	ModbusRTU_tx_buffer[4] = 0x00;
+	ModbusRTU_tx_buffer[5] = Quantity;
+	uint16_t CRC16 = ModbusRTU_CRC16_Calculate(ModbusRTU_tx_buffer, 6, Slave_byte_order);
+	ModbusRTU_tx_buffer[6] = (uint8_t) (CRC16 >> 8);
+	ModbusRTU_tx_buffer[7] = (uint8_t) (CRC16 << 8);
+}
+/*------------------------------Функция 00x03 Read_adress Analog Output Holding Registers----------------------------- */
 #endif /* SRC_MODBUSRTU_C_ */
--- ModbusRTU/ModbusRTU.h
+++ ModbusRTU/ModbusRTU.h
@@ -1,6 +1,9 @@
 /*
  * ModbusRTU.h
  *
+ *  Библиотека для работы по протоколу ModbusRTU в режиме Master.
+ *  Функции: формировать запрос, вычислять CRC16.
+ *
  *  Created on: Nov 16, 2021
  *      Author: Oleg Volkov
  */
@@ -11,5 +14,6 @@
 #include <main.h>
 
 uint16_t ModbusRTU_CRC16_Calculate(uint8_t *data, uint8_t lenght, uint8_t byte_order);
+void ModbusRTU_Read_Holding_Registers(uint8_t Slave_ID, uint16_t Read_adress, uint8_t Quantity, uint8_t Slave_byte_order);
 
 #endif /* INC_MODBUSRTU_H_ */
